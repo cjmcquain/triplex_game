d7d61e945ff8ac70cb22a18132bcc852096f216a
Charles McQuain <cmcquai1@my.westga.edu>
2019-05-27 20:54:06 -0700

    Added Welcome message and initialize variable, created gitignore

---
+++ TripleX.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main()
+{
+  std::cout << "You are a secret agent breaking into a secure server room.";
+  std::cout << std::endl;
+  std::cout << "Enter the correct code to continue...";
+
+  int a = 4;
+
+  std::cout << std::endl;
+  std::cout << a;
+
+  return 0;
+}
